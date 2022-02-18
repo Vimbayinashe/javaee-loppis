898fa5405dc9242428e7e1848d237c6ef0459a6a
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-18 09:38:32 +0100

    Create a demo object using builder pattern

---
+++ src/main/java/se/iths/entity/Demo.java
@@ -0,0 +1,13 @@
+package se.iths.entity;
+
+public class Demo {
+
+    private String name;
+    private int value;
+
+
+    public Demo(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+}
---
+++ src/main/java/se/iths/entity/DemoBuilder.java
@@ -0,0 +1,31 @@
+package se.iths.entity;
+
+public class DemoBuilder {
+    private String name;
+    private int value;
+
+    public DemoBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public DemoBuilder setValue(int value) {
+        this.value = value;
+        return this;
+    }
+
+    public DemoBuilder createDefaultPerson() {
+        this.name = "Martin";
+        this.value = 7;
+        return this;
+    }
+
+    public Demo build() {
+        return new Demo(name, value);
+    }
+
+    public static void main(String[] args) {
+        Demo person = new DemoBuilder().createDefaultPerson().build();
+        Demo person2 = new DemoBuilder().setName("Kalle").setValue(2).build();
+    }
+}
