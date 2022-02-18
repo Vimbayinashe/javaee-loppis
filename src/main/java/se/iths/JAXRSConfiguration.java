a6a883adbe094d07b393257f713ef3edc36ab6c0
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-18 08:34:14 +0100

    Update root endpoint

--- src/main/java/se/iths/JAXRSConfiguration.java
+++ src/main/java/se/iths/JAXRSConfiguration.java
@@ -3,7 +3,7 @@ package se.iths;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("api/v1/")
+@ApplicationPath("api/v1")
 public class JAXRSConfiguration extends Application {
 
 }
