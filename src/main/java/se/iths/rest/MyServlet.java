605f56967bda8ffbb073ee2517a49edaa4294002
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-02-21 09:05:11 +0100

    Add previously used MyServlet class example

---
+++ src/main/java/se/iths/rest/MyServlet.java
@@ -0,0 +1,21 @@
+package se.iths.rest;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
